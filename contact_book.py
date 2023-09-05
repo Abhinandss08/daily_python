974e8c42f60961cb92af279979ef7ed16bfd70ad
Abhinand S S <abhinand@yougotagift.com>
2023-09-05 17:19:06 +0530

    Contact book program completed

--- contact_book.py
+++ contact_book.py
@@ -29,6 +29,40 @@ def viewContact():
         print('_________________________________________________________')
 
 
+def searchContact():
+    search_name = input('Enter the name you want to search\t')
+    found_contacts = []
+
+    for contact in contact_book:
+        if search_name.lower() in contact['Name'].lower():
+            found_contacts.append(contact)
+
+    if found_contacts:
+        for index, contact in enumerate(found_contacts, start=1):
+            print(f"{index}.Name:{contact['Name']}, "
+                  f"Email:{contact['Email']}, Phone:{contact['Phone']}")
+            print('_________________________________________________________')
+    else:
+        print('Name nowhere to be found')
+        print('_________________________________________________________')
+
+
+def deleteContact():
+    delete_name = input('Enter the name you want to delete\t')
+    delete_status = False
+
+    for contact in contact_book:
+        if delete_name.lower() == contact['Name'].lower():
+            contact_book.remove(contact)
+            print(f"{delete_name} is deleted from contact_book")
+            print('_________________________________________________________')
+            delete_status = True
+            break
+    if not delete_status:
+        print(f'{delete_name} can not be found')
+        print('_________________________________________________________')
+
+
 while True:
     choice = int(input('1.Add Contact\n2.View contact\n3.Search contact\n'
                        '4.Delete contact\n5.Quit\nEnter your choice\t'))
@@ -36,6 +70,10 @@ while True:
         addContact()
     elif choice == 2:
         viewContact()
+    elif choice == 3:
+        searchContact()
+    elif choice == 4:
+        deleteContact()
     elif choice == 5:
         break
     else:
